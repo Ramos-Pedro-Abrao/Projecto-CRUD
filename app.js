f56bf6b8c49c3288fd28760568444298096588fa
Ramos Pedro Abrao <abrao2snake@gmail.com>
2026-08-31 15:08:54 +0100

    Criar produtos.

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const app = express();
 const database = require('./Database/database.js');
 const controller = require('./controller/controller.js');
 
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 async function criarTabela(){
   return await controller.criarTabela();
 }
 criarTabela();
-controller.inserirProdutos("teste");
+
+app.post("/api/inserirProdutos", (req, res)=>{
+  const produto = req.body;
+  controller.inserirProdutos(produto);
+});
 
 app.listen(3000, ()=>{
-  console.log("Servidor rodando com sucesso!");
+  console.log("Servidor rodando com sucesso em http://localhost:3000");
 });
